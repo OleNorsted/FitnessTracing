f1c4678e772ee8e7faa9a4be1586c009e8a53417
Ole Norsted <s351890@oslomet.no>
2021-09-15 22:43:52 +0200

    initial commit

--- src/main/java/ole/norsted/fitnesstracing/FitnessTracingApplication.java
+++ src/main/java/ole/norsted/fitnesstracing/FitnessTracingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitnessTracingApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(FitnessTracingApplication.class, args);
     }
