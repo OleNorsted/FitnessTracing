43a80c10314288c02f65529204c4557dd9c65b64
Ole Norsted <s351890@oslomet.no>
2021-09-15 22:35:39 +0200

    initial commit

---
+++ src/main/java/ole/norsted/fitnesstracing/FitnessTracingApplication.java
@@ -0,0 +1,13 @@
+package ole.norsted.fitnesstracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTracingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ole/norsted/fitnesstracing/FitnessTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package ole.norsted.fitnesstracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
